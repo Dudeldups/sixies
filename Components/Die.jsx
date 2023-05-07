e5a105adc8663eb83dce47447bebcce23f4c08ed
Dudeldups <arne.jacob@mail.de>
2023-05-08 00:53:24 +0200

    Added random numbers, onclick for dice

--- Components/Die.jsx
+++ Components/Die.jsx
@@ -1,3 +1,10 @@
-export default function Die() {
-  return <div className="die"></div>;
+export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "green" : "white",
+  };
+  return (
+    <div className="die" style={styles} onClick={props.clickDie}>
+      {props.value}
+    </div>
+  );
 }
--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,54 @@ import { useState } from "react";
 import Die from "../Components/Die.jsx";
 
 export default function App() {
-  const [dice, setDice] = useState(0);
+  const [dice, setDice] = useState(startGame());
   const [game, setGame] = useState({
     won: false,
     state: "Start",
     startTime: Date.now(),
   });
 
+  function startGame() {
+    let newDice = [];
+    for (let i = 0; i < 6; i++) {
+      newDice.push(createDie(i));
+    }
+    return newDice;
+  }
+
+  function createDie(id) {
+    return {
+      id,
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      activeRounds: 0,
+    };
+  }
+
+  function clickDie(die) {
+    setDice((prevDice) =>
+      prevDice.map((cur) =>
+        cur.id === die.id ? { ...die, isHeld: !die.isHeld } : cur
+      )
+    );
+  }
+
+  const diceElements = dice.map((die) => {
+    return (
+      <Die
+        key={die.id}
+        id={die.id}
+        value={die.value}
+        isHeld={die.isHeld}
+        activeRounds={die.activeRounds}
+        clickDie={() => clickDie(die)}
+      />
+    );
+  });
+
   return (
     <div className="container">
-      <div className="dice-grid">
-        <Die />
-        <Die />
-        <Die />
-        <Die />
-        <Die />
-        <Die />
-      </div>
+      <div className="dice-grid">{diceElements}</div>
       <button className="game-button">{game.state}</button>
     </div>
   );
